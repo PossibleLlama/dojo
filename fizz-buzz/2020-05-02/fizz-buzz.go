e4507f27c53b05df6b2aa69cbef822c4185e033e
Chris Collier <possiblellama@gmail.com>
2020-05-02 20:06:10 +0100

    fizz-buzz-golang 2020-05-02 20:06

---
+++ fizz-buzz/2020-05-02/fizz-buzz.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("start")
+}
+
+func FizzBuzz(number int) string {
+	return "1"
+}
---
+++ fizz-buzz/2020-05-02/fizz-buzz_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFunc(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected string
+	}{
+		{1, "1"},
+	}
+
+	for _, testItem := range tests {
+		testName := fmt.Sprintf("%d", testItem.input)
+		t.Run(testName, func(t *testing.T) {
+			actual := FizzBuzz(testItem.input)
+			if actual != testItem.expected {
+				t.Errorf("Got %s, expected %s", actual, testItem.expected)
+			}
+		})
+	}
+}
