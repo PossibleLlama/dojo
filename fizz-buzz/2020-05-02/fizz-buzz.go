ccab79810ed5364694f0a76e00e84c0358cd9c20
Chris Collier <possiblellama@gmail.com>
2020-05-02 21:56:28 +0100

    fizz-buzz-golang 2020-05-02 21:56

--- fizz-buzz/2020-05-02/fizz-buzz.go
+++ fizz-buzz/2020-05-02/fizz-buzz.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// IntReplacement used as a way to join a number to the string that replaces it
+// E.g. 3 -> "Fizz"
 type IntReplacement struct {
 	divisor     int
 	replacement string
@@ -23,17 +25,19 @@ func main() {
 	fmt.Println(ReplaceBetweenRange(1, 100, replacements, "\n"))
 }
 
-func appendIfDivisible(number int, replaceable IntReplacement) string {
+// AppendIfDivisible Check if the given number is divisible by passed divisor, and if so return the string instead
+func AppendIfDivisible(number int, replaceable IntReplacement) string {
 	if number%replaceable.divisor == 0 {
 		return replaceable.replacement
 	}
 	return ""
 }
 
+// GetStringForNumber For a given number, check each replacement string and replace as needed
 func GetStringForNumber(number int, replacements []IntReplacement) string {
 	output := ""
 	for _, element := range replacements {
-		output += appendIfDivisible(number, element)
+		output += AppendIfDivisible(number, element)
 	}
 
 	if len(output) > 0 {
@@ -42,6 +46,7 @@ func GetStringForNumber(number int, replacements []IntReplacement) string {
 	return fmt.Sprint(number)
 }
 
+// ReplaceBetweenRange For a starting number, list of replacements and a string to put between strings, put it all together
 func ReplaceBetweenRange(start, end int, replacements []IntReplacement, intermediary string) string {
 	output := ""
 	for i := start; i <= end; i++ {
