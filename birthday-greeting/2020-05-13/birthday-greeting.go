e21c7adb69e32e5850544b6b07f7c52fe5277a57
Chris Collier <possiblellama@gmail.com>
2020-05-13 19:42:13 +0100

    birthday-greeting-golang 2020-05-13 19:42

---
+++ birthday-greeting/2020-05-13/birthday-greeting.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Greetings friend!")
+}
+
+// MessageFromName Get happy birthday message for given person
+func MessageFromName(firstName string) string {
+	return "Happy birthday, dear !"
+}
---
+++ birthday-greeting/2020-05-13/birthday-greeting_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMessageFromName(t *testing.T) {
+	var tests = []struct {
+		input    string
+		expected string
+	}{
+		{"", "Happy birthday, dear !"},
+	}
+
+	for _, testItem := range tests {
+		testName := fmt.Sprintf("%s", testItem.input)
+		t.Run(testName, func(t *testing.T) {
+			actual := MessageFromName(testItem.input)
+			if actual != testItem.expected {
+				t.Errorf("Got %s, expected %s", actual, testItem.expected)
+			}
+		})
+	}
+}
