1324978267637e9c1976fe31851a35b8f1c406d9
chris.collier <christopher.collier@zoopla.co.uk>
2020-12-14 15:03:27 +0000

    Add function to find numbers that sum together

--- sum-to-x-then-multiply/2020-12-14/main.py
+++ sum-to-x-then-multiply/2020-12-14/main.py
@@ -14,3 +14,26 @@ def read_numbers_from_string(string):
             stringExceptions.append("{0} is not a number. Error {1}".format(element, err))
 
     return numberItems, stringExceptions
+
+def find_numbers_that_sum_to(sum, number_list):
+    sum_to = []
+    if len(number_list) == 0: pass
+    elif len(number_list) == 1:
+        if sum == number_list[0]:
+            sum_to.append([number_list[0]])
+        else: pass
+    else:
+        for x in sorted(number_list):
+            for y in sorted(number_list):
+                if x < y or (x + y) > sum:
+                    break
+                elif x == y and number_list.count(x) == 1:
+                    break
+                elif sum_to.count([x, x]) == 1:
+                    break
+                elif x + y == sum:
+                    sum_to.append(sorted([x, y]))
+            else:
+                continue
+
+    return sum_to
--- sum-to-x-then-multiply/2020-12-14/tests.py
+++ sum-to-x-then-multiply/2020-12-14/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from main import read_numbers_from_string
+from main import read_numbers_from_string, find_numbers_that_sum_to
 
 class TestReadNumbersFromString(unittest.TestCase):
     def test_empty_string(self):
@@ -53,11 +53,65 @@ class TestReadNumbersFromString(unittest.TestCase):
         self.assertEqual(numbers, [0,1,3])
         self.assertEqual(errors, [generate_error("a"), generate_error("b")])
 
+class TestFindNumbersThatSumTo(unittest.TestCase):
+    def test_empty_list(self):
+        sum = 0
+        input_list = []
+        numbers_that_sum_to = find_numbers_that_sum_to(sum, input_list)
+
+        self.assertEqual(len(numbers_that_sum_to), 0)
+
+    def test_single_item_list_equals_sum(self):
+        sum = 1
+        input_list = [1]
+        numbers_that_sum_to = find_numbers_that_sum_to(sum, input_list)
+
+        self.assertEqual(len(numbers_that_sum_to), 1)
+        self.assertEqual(numbers_that_sum_to[0], [sum])
+
+    def test_single_item_list_unequal_sum(self):
+        sum = 2
+        input_list = [1]
+        numbers_that_sum_to = find_numbers_that_sum_to(sum, input_list)
+
+        self.assertEqual(len(numbers_that_sum_to), 0)
+
+    def test_two_item_list_equals_sum(self):
+        sum = 3
+        input_list = [1,2]
+        numbers_that_sum_to = find_numbers_that_sum_to(sum, input_list)
+
+        assert_sum_list(self, sum, numbers_that_sum_to)
+        self.assertEqual(len(numbers_that_sum_to), 1)
+
+    def test_multiple_item_list_has_one_match(self):
+        sum = 10
+        input_list = [5,6,8,3,4]
+        numbers_that_sum_to = find_numbers_that_sum_to(sum, input_list)
+
+        assert_sum_list(self, sum, numbers_that_sum_to)
+        self.assertEqual(len(numbers_that_sum_to), 1)
+        self.assertEqual(numbers_that_sum_to[0], [4,6])
+
+    def test_multiple_items_list_has_duplicate(self):
+        sum = 10
+        input_list = [2,3,5,9,5,6]
+        numbers_that_sum_to = find_numbers_that_sum_to(sum, input_list)
+
+        assert_sum_list(self, sum, numbers_that_sum_to)
+        self.assertEqual(len(numbers_that_sum_to), 1)
+        self.assertEqual(numbers_that_sum_to[0], [5,5])
+
 def generate_error(string):
     return string + " is not a number. Error " + generate_int_parse_error(string)
 
 def generate_int_parse_error(string):
     return "invalid literal for int() with base 10: '{0}'".format(string)
+
+def assert_sum_list(self, sum, sum_list):
+    self.assertGreaterEqual(len(sum_list), 1)
+    for pair in sum_list:
+        self.assertEqual(pair[0] + pair[1], sum)
 
 if __name__ == '__main__':
     unittest.main()
