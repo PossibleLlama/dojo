f16f4b5fcfa446fca36ae4b5bbe064e84636372f
chris.collier <possiblellama@gmail.com>
2022-12-22 19:27:23 +0000

    prime-factors-notset 2022-12-22 19:27

--- prime-factors/2022-12-22/src/main.rs
+++ prime-factors/2022-12-22/src/main.rs
@@ -5,25 +5,31 @@ fn main() {
 fn is_prime(a: i32) -> bool {
     if a <= 1 {
         return false;
-    } else if a == 2 {
-        return true;
-    } else if a % 2 == 0 {
-        return false;
+    }
+    for n in 2..a-1 {
+        if a % n == 0 {
+            return false;
+        }
     }
     return true;
 }
 
 #[test]
-fn test_is_prime_under_10() {
+fn test_is_prime_even_under_10() {
     assert_eq!(is_prime(0), false);
-    assert_eq!(is_prime(1), false);
     assert_eq!(is_prime(2), true);
-    assert_eq!(is_prime(3), true);
     assert_eq!(is_prime(4), false);
-    assert_eq!(is_prime(5), true);
     assert_eq!(is_prime(6), false);
-    assert_eq!(is_prime(7), true);
     assert_eq!(is_prime(8), false);
+}
+
+#[test]
+fn test_is_prime_odd_under_10() {
+    assert_eq!(is_prime(1), false);
+    assert_eq!(is_prime(3), true);
+    assert_eq!(is_prime(5), true);
+    assert_eq!(is_prime(7), true);
+    assert_eq!(is_prime(9), false);
 }
 
 #[test]
