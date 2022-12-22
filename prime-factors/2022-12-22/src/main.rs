7e4d033ea5d4f2951a5e263b3daad2df8b1913e0
chris.collier <possiblellama@gmail.com>
2022-12-22 18:23:11 +0000

    prime-factors-notset 2022-12-22 18:23

--- prime-factors/2022-12-22/src/main.rs
+++ prime-factors/2022-12-22/src/main.rs
@@ -2,11 +2,19 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn is_prime(_a: i32, _b: i32) -> bool {
-    return false;
+fn is_prime(a: i32) -> bool {
+    if a == 1 {
+        return false;
+    }
+    return true;
 }
 
 #[test]
-fn test_single_num() {
-    assert_eq!(is_prime(1, 1), false);
+fn test_is_prime_single_num() {
+    assert_eq!(is_prime(1), false);
+}
+
+#[test]
+fn test_is_prime_two() {
+    assert_eq!(is_prime(2), true);
 }
