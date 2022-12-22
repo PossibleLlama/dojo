56c90d191c41a069a26035fafbcd55c7dafc5941
chris.collier <possiblellama@gmail.com>
2022-12-22 19:40:28 +0000

    prime-factors-notset 2022-12-22 19:40

--- prime-factors/2022-12-22/src/main.rs
+++ prime-factors/2022-12-22/src/main.rs
@@ -1,8 +1,10 @@
+use primes::{ is_prime };
+
 fn main() {
     println!("Hello, world!");
 }
 
-fn is_prime(a: i32) -> bool {
+fn is_a_prime(a: u64) -> bool {
     if a <= 1 {
         return false;
     }
@@ -16,23 +18,21 @@ fn is_prime(a: i32) -> bool {
 
 #[test]
 fn test_is_prime_under_10() {
-    assert_eq!(is_prime(0), false);
-    assert_eq!(is_prime(1), false);
-    assert_eq!(is_prime(2), true);
-    assert_eq!(is_prime(3), true);
-    assert_eq!(is_prime(4), false);
-    assert_eq!(is_prime(5), true);
-    assert_eq!(is_prime(6), false);
-    assert_eq!(is_prime(7), true);
-    assert_eq!(is_prime(8), false);
-    assert_eq!(is_prime(9), false);
+    assert_eq!(is_a_prime(0), false);
+    assert_eq!(is_a_prime(1), false);
+    assert_eq!(is_a_prime(2), true);
+    assert_eq!(is_a_prime(3), true);
+    assert_eq!(is_a_prime(4), false);
+    assert_eq!(is_a_prime(5), true);
+    assert_eq!(is_a_prime(6), false);
+    assert_eq!(is_a_prime(7), true);
+    assert_eq!(is_a_prime(8), false);
+    assert_eq!(is_a_prime(9), false);
 }
 
 #[test]
-fn test_is_prime_negative() {
-    assert_eq!(is_prime(-1), false);
-    assert_eq!(is_prime(-3), false);
-    assert_eq!(is_prime(-5), false);
-    assert_eq!(is_prime(-7), false);
-    assert_eq!(is_prime(-9), false);
+fn test_is_prime_under_1000() {
+    for n in 1..1000 {
+        assert_eq!(is_a_prime(n), is_prime(n));
+    }
 }
