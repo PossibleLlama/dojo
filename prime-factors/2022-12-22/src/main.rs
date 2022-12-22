38b0aedd03cd5dc3c7ac3f25bec53c40b3c0f564
chris.collier <possiblellama@gmail.com>
2022-12-22 18:28:24 +0000

    prime-factors-notset 2022-12-22 18:28

--- prime-factors/2022-12-22/src/main.rs
+++ prime-factors/2022-12-22/src/main.rs
@@ -15,6 +15,7 @@ fn is_prime(a: i32) -> bool {
 
 #[test]
 fn test_is_prime_under_10() {
+    assert_eq!(is_prime(0), false);
     assert_eq!(is_prime(1), false);
     assert_eq!(is_prime(2), true);
     assert_eq!(is_prime(3), true);
